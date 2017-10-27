986ad658441c6f1a31cb7c1301d2d96ddeec5968
Jonathan Mirabile <jonathanmirabile@mail.weber.edu>
2017-10-26 21:13:44 -0600

    Working through a connection bug with client. Unable to send messages from main but able to send messages during client initialization using wrapper methods

--- src/midterm/Client.java
+++ src/midterm/Client.java
@@ -1,5 +1,72 @@
 package midterm;
 
-public class Client {
+import java.io.*;
+import java.net.Socket;
 
-}
+import blackjack.message.ChatMessage;
+import blackjack.message.LoginMessage;
+import blackjack.message.MessageFactory;
+
+public class Client {
+	private int port = 8989;
+	private String URL = "ec2-54-91-0-253.compute-1.amazonaws.com";
+	private Socket client = null;
+    private ObjectOutputStream out = null;
+    private ObjectInputStream in = null;
+	
+	protected void startClient() {
+		try {
+			client = new Socket(URL, port);
+            out = new ObjectOutputStream(client.getOutputStream());
+            
+            System.out.println("Sending messages");
+            System.out.println("opening in.read");
+            in = new ObjectInputStream(client.getInputStream());
+            System.out.println("opened in.read");
+            
+    		sendLoginName("Jonathan");
+    		sendChatMessage("This is a test", "Jonathan");
+            System.out.println("Sent messages");
+
+            in.readObject();
+
+            System.out.println("Started client");
+		} catch (IOException e) {
+			System.out.println("Unable to connect to server");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+	protected void sendLoginName(String username) {
+		try {
+			LoginMessage Login = MessageFactory.getLoginMessage(username);
+			out.writeObject(Login);
+			out.flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	protected void sendChatMessage(String message, String userName) {
+		try {
+			ChatMessage Message = MessageFactory.getChatMessage(message, userName);
+			out.writeObject(Message);
+			out.flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		Client test = new Client();
+		test.startClient();
+		//test.sendLoginName("Jonathan");
+		test.sendChatMessage("Chat message 2", "Jonathan");
+	}
+	
+} // end class
