703e9950f8f4f8bbfd7f87dd02aee5f123a9cad4
Jonathan Mirabile <jonathanmirabile@mail.weber.edu>
2017-11-02 19:49:09 -0600

    Refactored client inititalization and added file for clientGUI

--- src/midterm/Client.java
+++ src/midterm/Client.java
@@ -8,53 +8,35 @@ import blackjack.message.LoginMessage;
 import blackjack.message.MessageFactory;
 
 public class Client {
+	
 	private int port = 8989;
-	private String URL = "ec2-54-91-0-253.compute-1.amazonaws.com";
+	private String URL = "ec2-54-172-123-164.compute-1.amazonaws.com";
 	private Socket client = null;
     private ObjectOutputStream out = null;
     private ObjectInputStream in = null;
-	
-	protected void startClient() {
-		try {
-			client = new Socket(URL, port);
-            System.out.println("opening output stream");
-            out = new ObjectOutputStream(client.getOutputStream());
-            System.out.println("opened output stream");
-            
-            System.out.println("opening input stream");
-            in = new ObjectInputStream(client.getInputStream());
-            System.out.println("opened input stream");
-            
-            System.out.println("Sending messages");
-    		sendLoginName("Jonathan");
-    		sendChatMessage("This is a test", "Jonathan");
-            System.out.println("Sent messages");
 
-            System.out.println("opening in.read");
-            in.readObject();
-            System.out.println("opened in.read");
-
-            System.out.println("Client started successfully");
-		} catch (IOException e) {
-			System.out.println("Client encountered an error during initialization");
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-			System.out.println("Client encountered an error during initialization");
-		}
-
-	}
-	
-	protected void sendLoginName(String username) {
-		try {
-			LoginMessage Login = MessageFactory.getLoginMessage(username);
+    public Client(String loginName) {
+        startClient(loginName); 
+    }
+    
+    protected void startClient(String loginName) {
+        try {
+        	client = new Socket(URL, port);
+        	out = new ObjectOutputStream(client.getOutputStream());
+        	in = new ObjectInputStream(client.getInputStream());
+        	
+        	System.out.println("Sending login message");
+			LoginMessage Login = MessageFactory.getLoginMessage(loginName);
 			out.writeObject(Login);
 			out.flush();
+			System.out.println("Sent login messages");
+                                 
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println("Client encountered an error during initialization");
 		}
-	}
-	
+             
+    }
+    
 	protected void sendChatMessage(String message, String userName) {
 		try {
 			ChatMessage Message = MessageFactory.getChatMessage(message, userName);
@@ -65,12 +47,14 @@ public class Client {
 			e.printStackTrace();
 		}
 	}
+    
+    protected void readMessageFromServer() {
+
+    }
 
 	public static void main(String[] args) {
-		Client test = new Client();
-		test.startClient();
-		//test.sendLoginName("Jonathan");
-		test.sendChatMessage("Chat message 2", "Jonathan");
-	}
+		Client test = new Client("Jonathan");
+		test.sendChatMessage("Chat message 1", "Jonathan");
+	} // end main
 	
 } // end class
---
+++ src/midterm/ClientGUI.java
@@ -0,0 +1,7 @@
+package midterm;
+
+public class ClientGUI {
+
+    
+
+}
